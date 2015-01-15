a52cee1128ec67ed90cbaad1ed425cf96062cc08
rudra101 <rudradeepmukherjee.mukherjee@gmail.com>
2015-01-15 06:40:47 +0530

    Initital Commit

--- flaskapp/app/package/models.py
+++ flaskapp/app/package/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
   __tablename__ = 'User'
   uid = db.Column(db.Integer, primary_key = True)
   uname = db.Column(db.String(30))
-  pwd = db.Column(db.String(200))
+  pwd = db.Column(db.String(45))
   email = db.Column(db.String(200), unique=True)
    
   def __init__(self, uname, password, email):
